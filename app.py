57f28ab95a8421eb5a72dd62cfda0016a58235b0
Gupir11 <45023168+Gupir11@users.noreply.github.com>
2025-12-19 11:16:26 +0000

    escritura app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from pickle import load
+import streamlit as st
+
+
+model = load(open('/workspaces/ML-web-app-using-Streamlit/models/nlp-url-spam.pkl', 'rb'))
+class_dict = {"0": "No Spam",
+              "1": "Spam"}
+
+st.title("Modelo Spam Prediccion")
+st.markdown("""Power by: [Guillermo Lugo]""")
+st.divider()
+
+val1 = st.text_input(label, value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, *, placeholder="ingrese el URL", disabled=False, label_visibility="visible", icon=None, width="stretch")
+
+
+if st.button("Prediccion"):
+    prediction = str(model.predict([[val1]])[0])
+    pred_class = class_dict[prediction]
+    st.divider()
+    st.write("Prediction:", pred_class)
+    st.divider()
